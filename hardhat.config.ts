9837e54c22749f46cc5b4fbd6c5a3d7158ed209b
NIC619 <twedusuck@gmail.com>
2021-08-26 18:02:14 +0800

    Add configs

---
+++ hardhat.config.ts
@@ -0,0 +1,38 @@
+// Plugins
+import "dotenv/config";
+// import "@nomiclabs/hardhat-waffle"
+import "@nomiclabs/hardhat-ethers";
+
+// This adds support for typescript paths mappings
+import "tsconfig-paths/register";
+
+const accounts = {
+  mnemonic:
+    process.env.MNEMONIC ||
+    "test test test test test test test test test test test junk",
+};
+const ALCHEMY_TOKEN = process.env.ALCHEMY_TOKEN || ""
+const INFURA_TOKEN = process.env.INFURA_TOKEN || ""
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1,
+      accounts,
+    },
+    goerli: {
+      chainId: 5,
+      url: `https://ropsten.infura.io/v3/${INFURA_TOKEN}`,
+      accounts,
+    },
+  },
+  solidity: {
+    version: "0.7.6",
+    settings: {
+        optimizer: {
+            enabled: true,
+            runs: 1000,
+        },
+    },
+  },
+};
