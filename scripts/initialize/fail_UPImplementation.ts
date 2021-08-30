dae849273c9e0f27728f6fe0db955cb6fced041f
NIC619 <twedusuck@gmail.com>
2021-08-30 11:21:35 +0800

    Fix: should be fail initialize script

--- scripts/initialize/fail_UPImplementation.ts
+++ scripts/initialize/fail_UPImplementation.ts
@@ -5,18 +5,14 @@ async function main() {
     const attacker = getAttacker()
 
     const UpgradeProxy = await ethers.getContractAt("UpgradeProxyImplementation", upgradeProxyAddr)
-    const operatorStored = await UpgradeProxy.callStatic.operator()
 
-    if (operatorStored == attacker.address) {
-        throw new Error(`Attacker is operator: ${operatorStored}`)
-    }
     const tx = await UpgradeProxy.connect(attacker).initialize(
         attacker.address,
         {
             gasLimit: 100000,
         }
     )
-    console.log(`initialize tx sent: ${tx.hash}`)
+    console.log(`fail initialize tx sent: ${tx.hash}`)
     await tx.wait()
 }
 
